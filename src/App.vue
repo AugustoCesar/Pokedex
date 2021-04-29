c730e1e327bdde377c5c4bcb7229b9843c5bec62
AugustoCesar <augustocesar.udi@gmail.com>
2021-04-29 17:13:43 -0400

    Inserindo imagens e sistema de busca

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,81 @@
 <template>
   <div id="app">
-    
-    <div class="column is-half is-offset-one-quarter">    
-      <div v-for="(poke,index) in pokemons" :key="index">
-        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/pokemon-logo.png" />
+      <hr />
+      <article class="message" id="pokedex">
+        <div class="message-header-center">
+          <h4 class="is-size-2">Pokedex</h4>
+        </div>
+        <div class="message-body-center">
+          <input
+            v-model="busca"
+            class="input is-rounded"
+            type="text"
+            placeholder="Buscar pokemon pelo nome"
+          />
+          <button
+            class="button is-rounded is-medium is-fullwidth is-primary"
+            id="btnBusca" @click="buscar"
+          >
+            Buscar
+          </button>
+        </div>
+      </article>
+
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import Pokemon from './components/Pokemon.vue';
+import axios from "axios";
+import Pokemon from "./components/Pokemon.vue";
 
 export default {
-  components: { Pokemon },
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
-      pokemons: []
-    }
+      pokemons: [],
+      filteredPokemons: [],
+      busca: "",
+    };
   },
-  created: function(){
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
-      console.log("Pegou a lista de pokemons");
-      this.pokemons = res.data.results;
-    })
-  }
-  
-}
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        console.log("Pegou a lista de pokemons");
+        this.pokemons = res.data.results;
+        this.filteredPokemons = res.data.results;
+      });
+  },
+  components: {
+    Pokemon,
+  },
+  methods: {
+    buscar: function () {
+      if (this.filteredPokemons == "" || this.filteredPokemons == " ") {
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter((pokemon) =>
+          pokemon.name.match(this.busca.toLowerCase())
+        );
+      }
+    },
+  },
+  computed: {
+    /* resultBusca: function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      } else{
+        return this.pokemons.filter(pokemon => pokemon.name.match(this.busca.toLowerCase()));
+      }
+    } */
+  },
+};
 </script>
 
 <style>
@@ -40,5 +86,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#btnBusca {
+  margin-top: 2%;
+}
+#pokedex {
+  padding: 3%;
 }
 </style>
